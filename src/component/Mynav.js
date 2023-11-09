47b12c627b2f5a614a6811e4e56f5263391e4c2e
rock's ezweb <alikerock@gmail.com>
2023-11-09 12:15:27 +0900

    app.js, Mynav.js 함수형변경

--- src/component/Mynav.js
+++ src/component/Mynav.js
@@ -1,43 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 
-class Mynav extends Component{
-  shouldComponentUpdate(newProps, newState){
-    console.log(
-      'shouldComponentUpdate 작동',
-      newProps.data, //변경된 값
-      this.props.data //변경전 값
-      );
-      if(this.props.data === newProps.data){
-        return false;
-      }
-    
-    return true;
-  }
-  render(){
-    console.log('Mynav 실행');
-    let lists = [];
-    let data = this.props.data;
-
+const Mynav = ({data, onChangePage})=>{
+  const[list, setList] = useState([]);
+  let lists = [];
+  const getList = ()=>{
     data.forEach( item =>{
       lists.push(
         <li key={item.id}>
           <a href="/"            
             onClick={e=>{
               e.preventDefault();
-                this.props.onChangePage(item.id);
+                onChangePage(item.id);
             }}          
           >{item.title}
           </a></li>
       );
     });
-    return(
-      <nav>
-        <ul>
-          {lists}
-        </ul>
-      </nav>
-    )
+    setList(lists);
   }
-}
+  
+  useEffect(()=>{
+    getList();
+    console.log('getList 실행');
+  }, [data]);
 
+  return(
+    <nav>
+      <ul>
+        {list}
+      </ul>
+    </nav>
+  )
+}
 export default Mynav;
